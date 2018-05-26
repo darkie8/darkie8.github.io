9b4d59a0c6d8213d56ff5927eee6237f388f025b
darkie9 <sdarkei9@gmail.com>
2018-05-27 01:43:23 +0530

    pretty

--- assignment_jq/Modal_component/A3.js
+++ assignment_jq/Modal_component/A3.js
@@ -1,15 +1,25 @@
-var n
+let n
+let m
 $(document).ready(function(){
     $(`#One`).click(function () { 
         $('.ModalS').removeClass("sicko").addClass("freako");
         $(`body`).addClass("Overflw");
         $(`.modalP`).animate({margin: "10vh auto auto auto",opacity: "1"},"fast");       
+        n= true;
+        
+        console.log(n)
+        
     });
-    $('.ModalS').click(function () { 
+    $(`.modalP`).click(function () { 
+        n= false
+        console.log(n)
+       });
+    $('.modalhelp').click(function () { 
         $('.ModalS').removeClass("freako").addClass("sicko");
         $(`body`).removeClass("Overflw")
         $(`.modalP`).css({"margin":"0vh auto auto auto","opacity": "0"})
     });
-
+    
+    console.log(n)
 
 })
