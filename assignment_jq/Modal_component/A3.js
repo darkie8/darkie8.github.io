b9acfee8ca9fc4811656ec4fd704ffcb9aaa0097
darkie9 <sdarkei9@gmail.com>
2018-05-31 00:44:38 +0530

    last update

--- assignment_jq/Modal_component/A3.js
+++ assignment_jq/Modal_component/A3.js
@@ -9,18 +9,26 @@ let marginUltMid= 10;
 let marginUltMre= 19;
 let marginUltCent;
 let marginUlt;
-let dataId;
+let dataId; // various variable declaration 
 $(document).ready(function(){
+    // clicking .show class button 
     $(`.show`).click(function () { 
-        dataId= $(this).attr("data-id-modal");
+        // storing different data attr inside variables to initiate certain modals
+        dataId= $(this).attr("data-id-modal"); 
         dataText= $(this).attr("data-size-text");
         console.log(dataText);
         modalPosition= $(this).attr("data-position-modal");
+
+        // height of modals
         modalHeight= $(`.${dataText}`).children("div.modalP").height();
         console.log(modalHeight);
         windowHeight= $(window).height()
         console.log(windowHeight)
-        marginUltCent= ((1-modalHeight/windowHeight)/2)*100
+
+        // center position of the modal
+        marginUltCent= ((1-modalHeight/windowHeight)/2)*100;
+
+        // conditioning
         marginUlt= (modalPosition=="centeraxis")?marginUltCent:((modalPosition=="normal-more")?
         marginUltMre:((modalPosition=="normal")?
         marginUltMid:marginUltSm));
