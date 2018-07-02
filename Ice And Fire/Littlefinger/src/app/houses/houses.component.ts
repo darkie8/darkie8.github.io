068791feaea6d3caffea4ff337cf767523f1c11c
darkie9 <sdarkei9@gmail.com>
2018-07-02 14:31:10 +0530

    hmm

--- Ice And Fire/Littlefinger/src/app/houses/houses.component.ts
+++ Ice And Fire/Littlefinger/src/app/houses/houses.component.ts
@@ -8,7 +8,9 @@ declare var $: any;
 })
 export class HousesComponent implements OnInit {
   public houses: any;
-  public beta = 'A';
+  public finalProduct: any;
+  public sortedProduct: any;
+  public sortedAlphabet: any;
   public alphabet = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase().split('');
   constructor(private httpHouse: HttpGOTService) {
     console.log('http request done');
@@ -49,14 +51,25 @@ export class HousesComponent implements OnInit {
         this.houses = data;
         // tslint:disable-next-line:prefer-const
         for (let x of this.houses) {
+          // tslint:disable-next-line:prefer-const
           let frstLtr = x.name.charAt(6);
           console.log(frstLtr);
         }
 
         // tslint:disable-next-line:prefer-const
-        let finalProduct = this.houses.filter(obj => {
+        this.finalProduct = this.alphabet.map(main => {
+          // tslint:disable-next-line:prefer-const
 
-        })
+          return this.houses.filter(obj => Object.is(obj.name.charAt(6), main));
+
+        });
+        console.log(this.finalProduct);
+        console.log(Object.values(this.finalProduct[0]));
+
+        this.sortedProduct = this.finalProduct.filter(obj => Object.values(obj).length !== 0);
+        console.log(this.sortedProduct);
+        this.sortedAlphabet = this.sortedProduct.map(obj => obj[0].name.charAt(6));
+        console.log(this.sortedAlphabet);
 
 
 
@@ -69,10 +82,7 @@ export class HousesComponent implements OnInit {
 
 
 
-    // tslint:disable-next-line:triple-equals
-    if ($('.cardB').height() <= 10) {
-      $('.cardB').css({ 'height': '0px', 'width': '0px' });
-    }
+
 
     console.log(this.alphabet);
 
